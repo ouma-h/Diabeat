d01771668de89084267153d575f1bbdf73c7a93b
Montasir Ghanmy <montassar.ghanmy@gmail.com>
2020-05-02 17:25:08 +0100

    Added AddMedication activity

---
+++ app/src/main/java/com/example/diabeat/AddMedication.java
@@ -0,0 +1,14 @@
+package com.example.diabeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMedication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_medication);
+    }
+}
