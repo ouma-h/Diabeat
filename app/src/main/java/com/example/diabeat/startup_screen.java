86d5a5117b23e7c53730463e100b1adf151991bd
Oumeima Hannachi <ouma-h@users.noreply.github.com>
2020-05-08 14:44:10 +0200

    add firebase to project

--- app/src/main/java/com/example/diabeat/startup_screen.java
+++ app/src/main/java/com/example/diabeat/startup_screen.java
@@ -1,5 +1,6 @@
 package com.example.diabeat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,9 @@ import android.os.Bundle;
 
 import java.util.Timer;
 import java.util.TimerTask;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class startup_screen extends AppCompatActivity {
 
@@ -16,6 +20,10 @@ public class startup_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup_screen);
+
+        //Check current user
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.addAuthStateListener(authStateListener);
 
         timer = new Timer();
         timer.schedule(new TimerTask() {
@@ -27,4 +35,22 @@ public class startup_screen extends AppCompatActivity {
             }
         }, 3000);
     }
+
+    FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+            if (firebaseUser == null) {
+                Intent intent = new Intent(startup_screen.this, activity_login.class);
+                startActivity(intent);
+                finish();
+            }
+            if (firebaseUser != null) {
+                Intent intent = new Intent(startup_screen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    };
 }
