f8bf190d1cfb1838d1985685b5c156708afa557b
Oumeima Hannachi <ouma-h@users.noreply.github.com>
2020-05-10 01:18:37 +0200

    Reset Password fully functional + app gradle config

--- app/src/main/java/com/example/diabeat/PasswordResetActivity.java
+++ app/src/main/java/com/example/diabeat/PasswordResetActivity.java
@@ -1,14 +1,63 @@
 package com.example.diabeat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PasswordResetActivity extends AppCompatActivity {
+
+    private EditText inputEmail;
+    private Button btnReset;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_reset);
+
+        inputEmail = findViewById(R.id.userName);
+        btnReset = findViewById(R.id.sendEmail);
+        auth = FirebaseAuth.getInstance();
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplication(), "Please enter your mail address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(PasswordResetActivity.this, "We sent you an e-mail", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(PasswordResetActivity.this, "Error sending a e-mail! Please try again.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+
+        });
+    }
+
+    public void NavigateLogIn(View v) {
+        Intent intent = new Intent(this, activity_login.class);
+        startActivity(intent);
     }
 }
