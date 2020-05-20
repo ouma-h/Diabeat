5de79ac8ed511f4a4565eedb435c2fb5ff3b6c0f
Oumeima Hannachi <ouma-h@users.noreply.github.com>
2020-05-20 15:44:08 +0200

    get doctor / get appointments

--- app/src/main/java/com/example/diabeat/apiBackend/AppointmentAPI.java
+++ app/src/main/java/com/example/diabeat/apiBackend/AppointmentAPI.java
@@ -7,8 +7,10 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface AppointmentAPI {
     @GET("appointements/")
-    Call<List<Appointment>> getAppointments();
+    Call<List<Appointment>> getAppointments(@Query("user_id") int user_id);
+
 }
--- app/src/main/java/com/example/diabeat/apiBackend/DoctorAPI.java
+++ app/src/main/java/com/example/diabeat/apiBackend/DoctorAPI.java
@@ -1,15 +1,18 @@
 package com.example.diabeat.apiBackend;
 
 import com.example.diabeat.models.Doctor;
-import com.example.diabeat.models.ModelProgram;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface DoctorAPI {
     @GET("doctors/")
     Call<List<Doctor>> getDoctors();
+
+    @GET("doctors/{id}")
+    Call<Doctor> getDoctor(@Path("id") int userId);
 }
